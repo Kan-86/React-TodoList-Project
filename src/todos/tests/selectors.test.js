9747315df8609184ebeee516ede64121162e63fa
Sub-Z3r0 <andersen19@gmail.com>
2021-01-25 04:21:43 +0100

    added selectors test

---
+++ src/todos/tests/selectors.test.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';
+import {getCompletedTodos} from '../selectors';
+
+describe('The getCompletedTodos selector', () => {
+    it('Returns only completed todos', () => {
+        const fakeTodos = [{
+            text: 'Say hello',
+            isCompleted: true,
+        }, {
+            text: 'Say goodbye',
+            isCompleted: false,
+        }, {
+            text: 'Climb Esja',
+            isCompleted: false,
+        }];
+        const expected = [{
+            text: 'Say hello',
+            isCompleted: true,
+        }];
+        const actual = getCompletedTodos.resultFunc(fakeTodos);
+        expect(actual).to.deep.equal(expected);
+    });
+});
